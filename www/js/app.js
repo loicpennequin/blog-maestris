4166bced81aa7da4eab4dca3fb9600faa20db79a
Loïc Pennequin <lo.pennequin@gmail.com>
2017-09-13 15:52:47 +0200

    _Homepage is now responsive _added some mixins and helper classes for responsiveness _added angular routes for the different sections, and created the according views and controllers

--- www/js/app.js
+++ www/js/app.js
@@ -5,4 +5,10 @@ app.config(function($routeProvider, $httpProvider){
     .when('/', {templateUrl: 'views/home.html', controller : 'mainCtrl'})
     .when('/login', {templateUrl: 'views/admin_login.html', controller : 'adminCtrl'})
     .when('/admin', {templateUrl: 'views/admin.html', controller : 'adminCtrl'})
+    .when('/about', {templateUrl: 'views/about.html', controller : 'blogCtrl'})
+    .when('/categories/:categorie', {templateUrl: 'views/posts_by_category.html', controller : 'blogCtrl'})
+    .when('/archives', {templateUrl: 'views/posts_by_date.html', controller : 'blogCtrl'})
+    .when('/contact', {templateUrl: 'views/contact.html', controller : 'blogCtrl'})
+    .when('/articles/:slug', {templateUrl: 'views/post.html', controller : 'blogCtrl'})
+    .otherwise('/');
 });
